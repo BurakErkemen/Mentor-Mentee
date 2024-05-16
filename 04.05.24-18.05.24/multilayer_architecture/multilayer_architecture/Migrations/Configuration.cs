fa8bdc6e93c958fdecad0306bd8a7ae80cec242a
BurakErkemen <burakfurkanerkemen@gmail.com>
2024-05-16 17:56:37 +0300

    Create Basic Minimum API
    Multilayer C#Console Basıc Experience

--- 04.05.24-18.05.24/multilayer_architecture/multilayer_architecture/Migrations/Configuration.cs
+++ 04.05.24-18.05.24/multilayer_architecture/multilayer_architecture/Migrations/Configuration.cs
@@ -13,11 +13,7 @@
         }
 
         protected override void Seed(multilayer_architecture.Model.Context context)
-        {
-            //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
         }
     }
 }
